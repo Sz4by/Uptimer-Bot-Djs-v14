5a7c59c8be578a8193d12831c7d645a14edd1644
Sz4by <szaby.korodi@yahoo.com>
2024-09-04 00:44:26 +0300

    Update settings.js

--- Assets/Config/settings.js
+++ Assets/Config/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
   REPL_USER: true, // true for repl users || false for vps users
   REPL_SETTINGS: {
-    EXPRESS: false,
-    AUTO_KILL: false
+    EXPRESS: true,
+    AUTO_KILL: true
   },
   COMMANDS_LOGS: true,
   COMMANDS_ERROR_LOGS: false,
