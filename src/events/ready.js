2f1ed8f4cc61782f9ba0633a7f317b48232a7328
Szaby <szaby.korodi@yahoo.com>
2025-03-07 09:30:32 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,7 +10,7 @@ module.exports = {
         { name: `${client.guilds.cache.size} Servers`, type: 2 }, // LISTENING
         { name: `${client.channels.cache.size} Channels`, type: 0 }, // PLAYING
         { name: `${client.users.cache.size} Users`, type: 3 }, // WATCHING
-        { name: `Sonic, a sündisznó 3`, type: 5 } // COMPETING
+        { name: `Mission: Impossible – Leszámolás`, type: 5 } // COMPETING
       ];
       const status = [
         'online',
